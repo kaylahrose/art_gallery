21d44b89a33b70bdfa57515c055220e9629dab7c
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-28 15:47:54 -0800

    Add parent index view

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,11 @@
+class ArtistsController < ApplicationController
+    def index
+        
+    end
+    # def create
+    #     artwork = Artwork.new({
+    #         created_at = Datetime.new
+    #         updated_at = 
+    #     })
+    # end
+end
