c618b7a5c800cd0e8242f20918abcd50ec091035
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-12-01 14:56:27 -0800

    Add artists show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,14 @@
 class ArtistsController < ApplicationController
-    def index
-        @artists = Artist.all
-    end
+  def index
+    @artists = Artist.all
+  end
     # def create
     #     artwork = Artwork.new({
     #         created_at = Datetime.new
     #         updated_at = 
     #     })
     # end
+  def show
+    @artist = Artist.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+require 'faker'
+
+# require 'pry'; binding.pry
+
+artists = 50.times {Artist.create!(
+            name: Faker::Artist.unique.name, 
+            active: [true, false].sample, 
+            popularity: rand(6)
+            )}
+
+artitsts.each do |artist|
+  artist.artworks.create!(
+    name: Faker::JapaneseMedia::StudioGhibli.unique.movie,
+    medium: Faker::Color.hex_color,
+    boolean: [true, false].sample,
+    value: rand(1_000_000)
+  )
+end
