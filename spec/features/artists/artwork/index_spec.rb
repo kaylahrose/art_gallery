73283e3ea45a2784a9aa9301f5add0f8e96aa759
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-12-01 19:58:48 -0800

    Add artists artworks show and view

--- spec/features/artists/artwork/index_spec.rb
+++ spec/features/artists/artwork/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Artists artworks index' do
     warhol = Artist.create!(name: "Warhol", active: true, popularity: 5)
       rothko = Artist.create!(name: "Rothko", active: true, popularity: 5)
       michaelangelo = Artist.create!(name: "Michaelangelo", active: true, popularity: 5)
-      painting = Artwork.create!(name: "Starry Night", medium: "oil", value: 9834527, for_sale: true, artist: warhol)
+      painting_1 = Artwork.create!(name: "Starry Night", medium: "oil", value: 9834527, for_sale: true, artist: warhol)
       painting_2 = Artwork.create!(name: "Mona Lisa", medium: "oil", value: 9834527, for_sale: true, artist: rothko)
       painting_3 = Artwork.create!(name: "The David", medium: "oil", value: 9834527, for_sale: true, artist: michaelangelo)
       painting_4 = Artwork.create!(name: "Untitled", medium: "oil", value: 9834527, for_sale: true, artist: rothko)
@@ -14,6 +14,24 @@ RSpec.describe 'Artists artworks index' do
       expect(page).to have_content(painting_2.name)
       expect(page).to have_content(painting_4.name)
   end
-  it 'shows each artworks attributes'
+
+  it 'shows each artworks attributes' do
+    warhol = Artist.create!(name: "Warhol", active: true, popularity: 5)
+    rothko = Artist.create!(name: "Rothko", active: true, popularity: 5)
+    michaelangelo = Artist.create!(name: "Michaelangelo", active: true, popularity: 5)
+    painting_1 = Artwork.create!(name: "Starry Night", medium: "oil", value: 9834527, for_sale: true, artist: warhol)
+    painting_2 = Artwork.create!(name: "Mona Lisa", medium: "oil", value: 9834527, for_sale: true, artist: rothko)
+    painting_3 = Artwork.create!(name: "The David", medium: "oil", value: 9834527, for_sale: true, artist: michaelangelo)
+    painting_4 = Artwork.create!(name: "Untitled", medium: "oil", value: 9834527, for_sale: true, artist: rothko)
+    visit "artists/#{rothko.id}/artworks"
+
+    expect(page).to have_content(painting_2.name)
+    expect(page).to have_content(painting_4.name)
+    expect(page).to have_content(painting_2.id)
+    expect(page).to have_content(painting_2.medium)
+    expect(page).to have_content(painting_2.value)
+    expect(page).to have_content(painting_2.for_sale)
+    expect(page).to_not have_content(painting_1.name)
+  end
   
 end
