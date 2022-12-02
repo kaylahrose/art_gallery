015db4c0eb717847175a1982a3673a9c6e7aea7d
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-12-01 17:29:15 -0800

    Test artwork index

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/artists', to: 'artists#index'
   get '/artists/:id', to: 'artists#show'
   get '/artworks', to: 'artworks#index'
+  
 end
--- spec/features/artists/index_spec.rb
+++ spec/features/artists/index_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'The Artists show page' do
+RSpec.describe 'The Artists index page' do
     it 'displays the artists' do
       warhol = Artist.create!(name: "Warhol", active: true, popularity: 5)
       rothko = Artist.create!(name: "Rothko", active: true, popularity: 5)
       painting = Artwork.create!(name: "Starry Night", medium: "oil", value: 9834527, for_sale: true, artist: warhol)
-      visit "/artists/#{warhol.id}"
+      visit "/artists"
 
       expect(page).to have_content(warhol.name)
-      expect(page).to have_content(warhol.active)
-      expect(page).to have_content(warhol.popularity)
+      expect(page).to have_content(rothko.name)
       expect(page).to_not have_content(painting.name)
-      expect(page).to_not have_content(rothko.name)
     end
 end
