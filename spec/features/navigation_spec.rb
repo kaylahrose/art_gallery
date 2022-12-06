bcc26ed03bbaf9703b35c2e70c954b0cbe0479ce
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-12-05 18:30:14 -0800

    Test navigation bar link all artists

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe "Artwork link" do
 
     expect(current_path).to eq('/artworks')
   end
+
+  it 'has a link to artist index on every page' do
+    visit "artworks" 
+    click_link 'All Artists'
+
+    expect(current_path).to eq('/artists')
+
+    warhol = Artist.create!(name: "Warhol", active: true, popularity: 5)
+    visit "/artists/#{warhol.id}"
+    click_link 'All Artists'
+
+    expect(current_path).to eq('/artists')
+  end
 end
