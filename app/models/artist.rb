f048f42bfe22fa60a303bccd6879fba117d761f2
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-12-05 17:03:59 -0800

    Create order by time created functionality

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,7 @@
 class Artist < ApplicationRecord
   has_many :artworks
+
+  def self.order_by_created_at
+    all.order(created_at: :desc)
+  end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Artist do
     warhol = Artist.create!(name: "Warhol", active: true, popularity: 5, created_at: (DateTime.now - 5.days))
     rothko = Artist.create!(name: "Rothko", active: true, popularity: 5, created_at: (DateTime.now - 4.days))
     michaelangelo = Artist.create!(name: "Michaelangelo", active: true, popularity: 5, created_at: DateTime.now)
-
-    expect(Artist.order_by_created_at).to eq([warhol, rothko,])
+    
+    expect(Artist.order_by_created_at).to eq([michaelangelo, rothko, warhol])
   end
 
   it 'displays created_at'
